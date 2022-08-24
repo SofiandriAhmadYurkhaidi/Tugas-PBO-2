9e437a154dffa9d4651d13ad9db05323605c006a
root <reksa.prayoga1012@gmail.com>
2022-08-24 10:30:20 +0700

    case and catch

--- index.php
+++ index.php
@@ -5,83 +5,83 @@ include 'pegawai.php';
 $db = new Database();
 $pegawai = new Pegawai();
 $dataPegawai = $pegawai->getAll($db->conn, 'SELECT * FROM pegawai');
-$title = "SEMUA DATA PEGAWAI";
+$title = 'SEMUA DATA PEGAWAI';
 if (isset($_GET['filter'])) {
-switch ($_GET['filter']) {
-    case '1':
-        $dataPegawai = $pegawai->getAll($db->conn, 'SELECT * FROM pegawai');
-        $title = "SEMUA DATA PEGAWAI";
-        break;
-    case '2':
-        $dataPegawai = $pegawai->getAll($db->conn, 'SELECT * FROM pegawai WHERE jenis_kelamin = "L"');
-        $title = "DATA PEGAWAI BERJENIS KELAMIN LAKI-LAKI";
-        break;
-    case '3':
-        $dataPegawai = $pegawai->getAll($db->conn, 'SELECT * FROM pegawai WHERE status = "M"');
-        $title = "DATA PEGAWAI DENGAN STATUS MENIKAH";
-        break;
-    case '4':
-        $dataPegawai= $pegawai->getAll($db->conn, 'SELECT * FROM pegawai WHERE status = "B"');
-        $title = "DATA PEGAWAI DENGAN STATUS BELUM MENIKAH";
-        break;
-    case '5':
-        $dataPegawai = $pegawai->getAll($db->conn, "SELECT * ,DATE_FORMAT(FROM_DAYS(DATEDIFF(now(),tgl_lahir)), '%Y') + 0 AS age from pegawai");
-        $data = [];
-        foreach ($dataPegawai as $key => $value) {
-            if ($value['age'] >= 20 && $value['age'] <= 30) {
-                $data[] = $value;
+    switch ($_GET['filter']) {
+        case '1':
+            $dataPegawai = $pegawai->getAll($db->conn, 'SELECT * FROM pegawai');
+            $title = 'SEMUA DATA PEGAWAI';
+            break;
+        case '2':
+            $dataPegawai = $pegawai->getAll($db->conn, 'SELECT * FROM pegawai WHERE jenis_kelamin = "L"');
+            $title = 'DATA PEGAWAI BERJENIS KELAMIN LAKI-LAKI';
+            break;
+        case '3':
+            $dataPegawai = $pegawai->getAll($db->conn, 'SELECT * FROM pegawai WHERE status = "M"');
+            $title = 'DATA PEGAWAI DENGAN STATUS MENIKAH';
+            break;
+        case '4':
+            $dataPegawai = $pegawai->getAll($db->conn, 'SELECT * FROM pegawai WHERE status = "B"');
+            $title = 'DATA PEGAWAI DENGAN STATUS BELUM MENIKAH';
+            break;
+        case '5':
+            $dataPegawai = $pegawai->getAll($db->conn, "SELECT * ,DATE_FORMAT(FROM_DAYS(DATEDIFF(now(),tgl_lahir)), '%Y') + 0 AS age from pegawai");
+            $data = [];
+            foreach ($dataPegawai as $key => $value) {
+                if ($value['age'] >= 20 && $value['age'] <= 30) {
+                    $data[] = $value;
+                }
             }
-        }
-        $dataPegawai = $data;
-        $title = "DATA PEGAWAI YANG BERUSIA DIANTARA 20 SAMPAI 30 TAHUN";
-        break;
-    case '6':
-        $dataPegawai = $pegawai->getAll($db->conn, "SELECT * ,DATE_FORMAT(FROM_DAYS(DATEDIFF(now(),tgl_lahir)), '%Y') + 0 AS age from pegawai");
-        $data = [];
-        foreach ($dataPegawai as $key => $value) {
-            if ($value['age'] >= 31 && $value['age'] <= 45) {
-                $data[] = $value;
+            $dataPegawai = $data;
+            $title = 'DATA PEGAWAI YANG BERUSIA DIANTARA 20 SAMPAI 30 TAHUN';
+            break;
+        case '6':
+            $dataPegawai = $pegawai->getAll($db->conn, "SELECT * ,DATE_FORMAT(FROM_DAYS(DATEDIFF(now(),tgl_lahir)), '%Y') + 0 AS age from pegawai");
+            $data = [];
+            foreach ($dataPegawai as $key => $value) {
+                if ($value['age'] >= 31 && $value['age'] <= 45) {
+                    $data[] = $value;
+                }
             }
-        }
-        $dataPegawai = $data;
-        $title = "DATA PEGAWAI YANG BERUSIA DIANTARA 31 SAMPAI 45 TAHUN";
-        break;
-    case '7':
-        $dataPegawai = $pegawai->getAll($db->conn, "SELECT * ,DATE_FORMAT(FROM_DAYS(DATEDIFF(now(),tgl_lahir)), '%Y') + 0 AS age from pegawai");
-        $data = [];
-        foreach ($dataPegawai as $key => $value) {
-            if ($value['mulai_kerja'] >= 1 && $value['mulai_kerja'] <= 5) {
-                $data[] = $value;
+            $dataPegawai = $data;
+            $title = 'DATA PEGAWAI YANG BERUSIA DIANTARA 31 SAMPAI 45 TAHUN';
+            break;
+        case '7':
+            $dataPegawai = $pegawai->getAll($db->conn, "SELECT * ,DATE_FORMAT(FROM_DAYS(DATEDIFF(now(),tgl_lahir)), '%Y') + 0 AS age from pegawai");
+            $data = [];
+            foreach ($dataPegawai as $key => $value) {
+                if ($value['mulai_kerja'] >= 1 && $value['mulai_kerja'] <= 5) {
+                    $data[] = $value;
+                }
             }
-        }
-        $dataPegawai = $data;
-        $title = "DATA PEGAWAI YANG MASA KERJANYA DIANTARA 1 SAMPAI 5 TAHUN";
-        break;
-    case '8':
-        $dataPegawai = $pegawai->getAll($db->conn, "SELECT * ,DATE_FORMAT(FROM_DAYS(DATEDIFF(now(),tgl_lahir)), '%Y') + 0 AS mulai_kerja from pegawai");
-        $data = [];
-        foreach ($dataPegawai as $key => $value) {
-            if ($value['mulai_kerja'] >= 6 && $value['mulai_kerja'] <= 10) {
-                $data[] = $value;
+            $dataPegawai = $data;
+            $title = 'DATA PEGAWAI YANG MASA KERJANYA DIANTARA 1 SAMPAI 5 TAHUN';
+            break;
+        case '8':
+            $dataPegawai = $pegawai->getAll($db->conn, "SELECT * ,DATE_FORMAT(FROM_DAYS(DATEDIFF(now(),tgl_lahir)), '%Y') + 0 AS mulai_kerja from pegawai");
+            $data = [];
+            foreach ($dataPegawai as $key => $value) {
+                if ($value['mulai_kerja'] >= 6 && $value['mulai_kerja'] <= 10) {
+                    $data[] = $value;
+                }
             }
-        }
-        $dataPegawai = $data;
-        $title = "DATA PEGAWAI YANG MASA KERJANYA DIANTARA 6 SAMPAI 10 TAHUN";
-        break;
-    case '9':
-        $dataPegawai = $pegawai->getAll($db->conn, "SELECT * ,DATE_FORMAT(FROM_DAYS(DATEDIFF(now(),tgl_lahir)), '%Y') + 0 AS mulai_kerja from pegawai");
-        $data = [];
-        foreach ($dataPegawai as $key => $value) {
-            if ($value['mulai_kerja'] >= 11 && $value['mulai_kerja'] <= 25) {
-                $data[] = $value;
+            $dataPegawai = $data;
+            $title = 'DATA PEGAWAI YANG MASA KERJANYA DIANTARA 6 SAMPAI 10 TAHUN';
+            break;
+        case '9':
+            $dataPegawai = $pegawai->getAll($db->conn, "SELECT * ,DATE_FORMAT(FROM_DAYS(DATEDIFF(now(),tgl_lahir)), '%Y') + 0 AS mulai_kerja from pegawai");
+            $data = [];
+            foreach ($dataPegawai as $key => $value) {
+                if ($value['mulai_kerja'] >= 11 && $value['mulai_kerja'] <= 25) {
+                    $data[] = $value;
+                }
             }
-        }
-        $dataPegawai = $data;
-        $title = "DATA PEGAWAI YANG MASA KERJANYA DIANTARA 11 SAMPAI 25 TAHUN";
-        break;
-    default:
-        $dataPegawai = $pegawai->getAll($db->conn, 'SELECT * FROM pegawai');
-        break;
+            $dataPegawai = $data;
+            $title = 'DATA PEGAWAI YANG MASA KERJANYA DIANTARA 11 SAMPAI 25 TAHUN';
+            break;
+        default:
+            $dataPegawai = $pegawai->getAll($db->conn, 'SELECT * FROM pegawai');
+            break;
     }
 }
 
@@ -102,13 +102,13 @@ switch ($_GET['filter']) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <title>DaWai!</title>
 </head>
 
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
+            <a class="navbar-brand" href="#">DaWai</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="Toggle navigation">
@@ -131,10 +131,12 @@ switch ($_GET['filter']) {
         </div>
     </nav>
     <div class="container">
-        <center><h2><?= $title; ?></h2></center>
-<br>
-<br>
-<br>
+        <center>
+            <h2><?= $title ?></h2>
+        </center>
+        <br>
+        <br>
+        <br>
         <div class="d-flex justify-content-end">
             <form class="d-flex gap-3" action="" method="GET">
                 <select name="filter" id="" class="form-select">
@@ -156,7 +158,7 @@ switch ($_GET['filter']) {
         <br>
         <table class="table table-bordered">
             <thead>
-                <tr>
+                <tr style="background:#91A6FF;">
                     <th scope="col">#</th>
                     <th scope="col">NIP</th>
                     <th scope="col">Nama</th>
@@ -166,6 +168,10 @@ switch ($_GET['filter']) {
                 </tr>
             </thead>
             <tbody>
+                <?php
+                try {
+                    if(mysqli_num_rows($dataPegawai) > 0){
+                ?>
                 <?php foreach($dataPegawai as $key => $pegawai){ ?>
 
                 <tr>
@@ -178,6 +184,38 @@ switch ($_GET['filter']) {
                 </tr>
 
                 <?php } ?>
+                <?php  } else { ?>
+
+                <tr>
+                    <td colspan="6" style="text-align:center;font-weight:bold;">Data tidak ditemukan</td>
+                </tr>
+
+                <?php }
+                } catch (\Throwable $th) {
+    if (count($dataPegawai) > 0) {
+        ?>
+
+                <?php foreach($dataPegawai as $key => $pegawai){ ?>
+
+                <tr>
+                    <td><?= $pegawai['id'] ?></td>
+                    <td><?= $pegawai['nip'] ?></td>
+                    <td><?= $pegawai['nama'] ?></td>
+                    <td><?= $pegawai['jenis_kelamin'] ?></td>
+                    <td><?= $pegawai['tgl_lahir'] ?></td>
+                    <td><?= $pegawai['status'] ?></td>
+                </tr>
+
+                <?php } ?>
+
+                <?php } else { ?>
+                <tr>
+                    <td colspan="6" style="text-align:center;font-weight:bold;">Data tidak ditemukan</td>
+                </tr>
+
+
+                <?php }
+                } ?>
             </tbody>
         </table>
 
